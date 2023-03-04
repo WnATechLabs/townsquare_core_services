fbfc54682a033d467f0e47650bd56731eeb8afbc
Will <william@wnatechlabs.com>
2023-03-04 13:22:13 -0400

    update .env validation

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,9 +1,17 @@
-import { cleanEnv, port, str } from 'envalid';
+import { cleanEnv, port, str, host } from 'envalid';
 
 function validateEnv() {
   cleanEnv(process.env, {
-    NODE_ENV: str(),
+    NODE_ENV: str({ choices: ['test', 'development', 'staging', 'production']}),
     PORT: port(),
+    DB_HOST: host(),
+    DB_USER: str(),
+    DB_PASSWORD: str(),
+    DB_DATABASE: str(),
+    SECRET_KEY: str(),
+    LOG_FORMAT: str(),
+    LOG_DIR: str(),
+    ORIGIN: str()
   });
 }
 
