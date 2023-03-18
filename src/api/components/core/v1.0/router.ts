5bb7052f022936794b0bb4b5d8307587f0f641d0
Will <william@wnatechlabs.com>
2023-03-18 12:27:10 -0400

    add ping route to api gateway

--- src/api/components/core/v1.0/router.ts
+++ src/api/components/core/v1.0/router.ts
@@ -1,20 +1,26 @@
 import { Router } from 'express';
 import { CustomRouterI } from '@shared/interfaces';
+
+import { CoreController } from './controller'
 import { Users, Auth } from './components'
 
 export class CoreRouter implements CustomRouterI {
     public path = '/api/v1.0';
     public router = Router();
+    protected controller: CoreController;
 
     protected usersRouter: Users.Router;
     protected authRouter: Auth.Router;
 
     constructor() {
+        this.controller = new CoreController()
         this.initializeRoutes();
     }
 
     private initializeRoutes() {
         this.router.use(`${this.path}/auth`, this.authRouter.router);
         this.router.use(`${this.path}/users`, this.usersRouter.router);
+
+        this.router.post(`/ping`, this.controller.ping);
     }
 }
