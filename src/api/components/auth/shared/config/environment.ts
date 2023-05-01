9cf499ee2b6c1dfe2e6f0ef6be620dcd83f8f4d4
Will <william@wnatechlabs.com>
2023-05-01 16:32:45 -0400

    update env vars management

---
+++ src/api/components/auth/shared/config/environment.ts
@@ -0,0 +1,10 @@
+import * as envVars from "../../../.././../environment";
+
+export const {
+  NODE_ENV,
+  DB_AUTH_PORT,
+  DB_AUTH_HOST,
+  DB_AUTH_USER,
+  DB_AUTH_PASS,
+  DB_AUTH_DATABASE,
+} = envVars;
